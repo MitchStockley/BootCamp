39449edd76e710b3507c8e3d2f3f05c8ce6a29da
Mitchell <mitchell.stockley@gmail.com>
2023-07-19 17:56:14 +0200

    Updated code

---
+++ Java Script/Advanced Javascript/Chapter2-Projects/project1.js
@@ -0,0 +1,19 @@
+//Miles-to-kilometers converter
+let miles = 130;
+let km = miles * 1.60934;
+
+console.log("The distance of", miles , "kms", "is equal to", km, "miles");
+
+
+//BMI Calculator Project
+
+let inches = 300;
+let pounds = 70;
+
+let height = inches * 2.54;
+let weight = pounds / 2.2046;
+
+console.log(height,weight);
+
+let bmi = weight / (height/100*height/100);
+console.log(bmi);
