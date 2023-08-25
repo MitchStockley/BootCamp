7bd2ae327a41310d71faee3d5168a1edec52493c
Mitchell <mitchell.stockley@gmail.com>
2023-08-25 11:54:23 +0200

    Added validators to the subscriber schema

--- nodeJS/Unit4/Lesson17/recipe_app/models/subscriber.js
+++ nodeJS/Unit4/Lesson17/recipe_app/models/subscriber.js
@@ -1,10 +1,23 @@
 "use strict";
 
 const mongoose = require("mongoose"),
+//adding validators to the subscriber schema 
   subscriberSchema = mongoose.Schema({
-    name: String,
-    email: String,
-    zipCode: Number
+    name: { //requiring the name property
+      type: String,
+      required: true
+    },
+    email: {
+      type: String,
+      required: true,
+      lowercase: true,
+      unique: true
+      },
+    zipCode: {
+      type: Number,
+      min: [10000, "Zip code too short"],
+      max: 99999
+    }
   });
 
 module.exports = mongoose.model("Subscriber", subscriberSchema);
