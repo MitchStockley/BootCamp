75bcdd7f53bac6fb163e1523b0e5042fa4ca2b7f
Mitchell <mitchell.stockley@gmail.com>
2023-09-22 09:16:23 +0200

    updated code

--- nodeJS/Bonus Projects/Chatbot/script.js
+++ nodeJS/Bonus Projects/Chatbot/script.js
@@ -5,7 +5,7 @@ const chatInput = document.querySelector(".chat-input textarea");
 const sendChatBtn = document.querySelector(".chat-input span");
 
 let userMessage = null; // Variable to store user's message
-const API_KEY = "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"; // Paste your API key here
+const API_KEY = ""; // Paste your API key here
 const inputInitHeight = chatInput.scrollHeight;
 
 const createChatLi = (message, className) => {
@@ -62,7 +62,7 @@ const handleChat = () => {
         chatbox.appendChild(incomingChatLi);
         chatbox.scrollTo(0, chatbox.scrollHeight);
         generateResponse(incomingChatLi);
-    }, 600);
+    }, 300);
 }
 
 chatInput.addEventListener("input", () => {
