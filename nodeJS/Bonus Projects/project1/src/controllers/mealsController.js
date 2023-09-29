21b6ff7ddc0f2b03101847cec0e2a1f0562ff9bb
Mitchell <mitchell.stockley@gmail.com>
2023-09-29 15:00:43 +0200

    File organising

--- nodeJS/Bonus Projects/project1/src/controllers/mealsController.js
+++
@@ -1,9 +0,0 @@
-//import meals model for the database
-const mealModel = require("../models/mealModel");
-//menu controller takes the request, response and next object 
-exports.getMenuController = (req, res, next) => {
-  //Get menu meals from model (Array)
-  const meals = mealModel.getMeals();
-
-  res.render("menu", { meals });
-};
